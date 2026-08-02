fe5929af3d3929e57130e30e6f6f74df35af1a6b
rikka <admin@rikka.cc>
2026-08-03 02:11:32 +0800

    feat: replace MantineProvider with QueryClientProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,20 @@
-import { createTheme, DEFAULT_THEME, MantineProvider, mergeMantineTheme } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
+import "./globals.css";
+
+const queryClient = new QueryClient();
 
 const rootEl = document.getElementById("root");
 
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
-  const theme = mergeMantineTheme(
-    DEFAULT_THEME,
-    createTheme({
-      primaryColor: "teal",
-      colors: {
-        teal: [
-          "#e6fcf5",
-          "#c3fae8",
-          "#96f2d7",
-          "#63e6be",
-          "#38d9a9",
-          "#20c997",
-          "#0ca678",
-          "#099268",
-          "#087f5b",
-          "#066649",
-        ],
-      },
-    }),
-  );
   root.render(
     <React.StrictMode>
-      <MantineProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </MantineProvider>
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 }
